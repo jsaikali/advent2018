55b4430630c2be4e9e46ebf04e7ce5cb47687226
jsaikali <jsaikali@LAMU1MSG47LH3QF.uhc.com>
2018-12-06 18:10:40 -0500

    day 3 complete

--- day3.py
+++ day3.py
@@ -21,4 +21,19 @@ def overlaps(claim_filepath):
 	num_duplicated=[x for x in list(collections.Counter(content_array_unnested).values()) if x>1]
 	return len(num_duplicated)
 
-a = overlaps('data/day3.txt')
+overlaps('data/day3.txt')
+
+def doesnt_overlap(claim_filepath):
+	with open(claim_filepath) as f:
+		content = f.readlines()
+	content=[x.strip() for x in content]
+	content_array = list([get_data(x) for x in content])
+	content_array_unnested=list(itertools.chain(*content_array))
+	duplicated = set(list(dict((key,value) for key, value in collections.Counter(content_array_unnested).items() if value != 1).keys()))
+	list_no_overlap=[]
+	for i in range(len(content_array)):
+		if len(set(content_array[i]).intersection(duplicated))==0:
+			list_no_overlap.append(content[i])
+	return list_no_overlap
+
+doesnt_overlap('data/day3.txt')
